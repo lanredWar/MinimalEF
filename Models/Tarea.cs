c688be4b4978d51ca3c9e928b2cc9a714ecbe8ef
Jonathanb <jonathanbernale@gmail.com>
2024-07-03 22:49:17 -0600

    Se agrega configuracion de restricciones mediante DataNotations en los modelos

--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,12 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MinimalEF.Models;
 public class Tarea
 {
+    [Key]
     public Guid TareaId { get; set; } 
+
+    [ForeignKey("CategoriaId")]
     public Guid CategoriaId { get; set; }
+
+    [Required]
+    [MaxLength(200)]
     public string Titulo { get; set; } 
     public Prioridad PrioridadTarea { get; set; }
     public DateTime FechaCreacion { get; set; }
     public virtual Categoria Categoria { get; set; }
+    
+    [NotMapped]
+    public string Resumen { get; set; }
 }
 public enum Prioridad
 {
--- TareasContext.cs
+++ TareasContext.cs
@@ -4,4 +4,6 @@ namespace MinimalEF.Models;
 public class TareasContext: DbContext
 {
     public DbSet<Categoria> Categorias {get; set;}
+    public DbSet<Tarea> Tareas {get; set;}
+    public TareasContext(DbContextOptions<TareasContext> options) : base(options){}
 }
