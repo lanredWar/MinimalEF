4047dad59cc76cce630dbf7294f2a223d60cd549
Jonathanb <jonathanbernale@gmail.com>
2024-07-04 22:33:06 -0600

    Se Eliminan los datanotations y se cambia la configuracion de los modelos con Fluent API para su mapeo

--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -4,20 +4,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace MinimalEF.Models;
 public class Tarea
 {
-    [Key]
+    // [Key]
     public Guid TareaId { get; set; } 
 
-    [ForeignKey("CategoriaId")]
+    // [ForeignKey("CategoriaId")]
     public Guid CategoriaId { get; set; }
 
-    [Required]
-    [MaxLength(200)]
+    // [Required]
+    // [MaxLength(200)]
     public string Titulo { get; set; } 
+    public string Descripcion{ get; set;}
     public Prioridad PrioridadTarea { get; set; }
     public DateTime FechaCreacion { get; set; }
     public virtual Categoria Categoria { get; set; }
     
-    [NotMapped]
+    // [NotMapped]
     public string Resumen { get; set; }
 }
 public enum Prioridad
