3d96a13589edf070f483f472fbc8c0fbcd4b46d4
Jonathanb <jonathanbernale@gmail.com>
2024-07-24 13:06:34 -0600

    Se agrega endpoint para actualizar registros dentro de la base de datos con EF

--- Program.cs
+++ Program.cs
@@ -39,4 +39,20 @@ app.MapPost("/api/tareas", async([FromServices] TareasContext dbContext, [FromBo
     return Results.Ok();
 });
 
+app.MapPut("/api/tareas/{id}", async([FromServices] TareasContext dbContext, [FromBody] Tarea tarea, [FromRoute] Guid id) =>
+{
+    var tareaActual = dbContext.Tareas.Find(id);
+    if (tareaActual != null)
+    {
+        tareaActual.Titulo = tarea.Titulo;
+        tareaActual.PrioridadTarea = tarea.PrioridadTarea;
+        tareaActual.Descripcion = tarea.Descripcion;
+
+        await dbContext.SaveChangesAsync();
+        return Results.Ok();
+    }
+
+    return Results.NotFound();
+});
+
 app.Run();
